661cfa03f2ea50dfb633ef4606f6381492db0492
NagisaY <sanpon0821ny@gmail.com>
2020-05-29 01:07:16 +0900

    タイトル追加

---
+++ Assets/Scripts/TitleSceneManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneManager : MonoBehaviour
+{
+    int counter = 0;
+    public GameObject ruleImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown && counter == 0)
+        {
+            counter++;
+            Debug.Log("anyKeyDown");
+            ruleImage.gameObject.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && counter == 1)
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+}

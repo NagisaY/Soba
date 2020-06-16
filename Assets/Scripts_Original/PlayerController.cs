e01b4e87bb68f0eaec2e546b3542696640aa0c52
NagisaY <sanpon0821ny@gmail.com>
2020-06-16 22:06:55 +0900

    ランキング機能追加

---
+++ Assets/Scripts_Original/PlayerController.cs
@@ -0,0 +1,286 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameManager gameManager;
+    public joyConTest _joyConTest;
+
+    private Vector3 setPos;
+    //食べた累計
+    public static int eatCount = 0;
+    //スコア代入用
+    public int scoreCount;
+    //MAX５ たまったらまんぷく
+    public int manpukuCount = 0;
+
+    public Slider slider;
+    public Text manpukuText;
+    public GameObject atodekeshitaiYatsu;
+    public GameObject osumousan;
+    public GameObject owans_5;
+    public GameObject owans_10;
+    public GameObject owans_15;
+    public GameObject owans_25;
+    public GameObject owans_35;
+    public GameObject owans_46;
+
+    private AudioSource sound01;
+    private AudioSource sound02;
+    private AudioSource sound03;
+    private AudioSource sound04;
+    private AudioSource sound05;
+
+    public bool MoveLock = false;
+    public bool isPlaying = false;
+    bool KeyMode = false;
+    bool JoyConMode = false;
+    bool manpukuSoundPlay = false;
+
+    public enum PlayerState
+    {
+        Center,
+        Right,
+        Left,
+        Manpuku,
+        Drink,
+        Eat_Right,
+        Eat_Left,
+        Hot
+    }
+
+    public PlayerState currentPlayerState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        setPos = this.transform.position;
+        slider.maxValue = 5;
+        slider.value = manpukuCount;
+
+        AudioSource[] audioSources = GetComponents<AudioSource>();
+        sound01 = audioSources[0];
+        sound02 = audioSources[1];
+        sound03 = audioSources[2];
+        sound04 = audioSources[3];
+        sound05 = audioSources[4];
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(currentPlayerState);
+        if (isPlaying == false)
+        {
+            eatCount = 0;
+            if (Input.GetKey(KeyCode.K))
+            {
+                sound04.PlayOneShot(sound04.clip);
+                KeyMode = true;
+                atodekeshitaiYatsu.gameObject.SetActive(false);
+                isPlaying = true;
+
+            }
+            if (Input.GetKey(KeyCode.J))
+            {
+                sound04.PlayOneShot(sound04.clip);
+                JoyConMode = true;
+                atodekeshitaiYatsu.gameObject.SetActive(false);
+                isPlaying = true;
+            }
+
+        }
+
+        if (KeyMode == true)
+        {
+            KeyPlayMode();
+
+        }
+        if (JoyConMode == true)
+        {
+            JoyConPlayMode();
+
+        }
+
+        if (isPlaying == true)
+        {
+            //まんぷくゲージ
+            //スライダー表示
+            slider.gameObject.SetActive(true);
+            //テキスト表示
+            manpukuText.gameObject.SetActive(true);
+            manpukuText.text = (manpukuCount + " / 5");
+            //Max5
+            slider.value = manpukuCount;
+            //スコア代入
+            scoreCount = eatCount;
+            //Debug.Log(eatCount);
+
+            if (eatCount >= 5) owans_5.gameObject.SetActive(true);
+            if (eatCount >= 10) owans_10.gameObject.SetActive(true);
+            if (eatCount >= 15) owans_15.gameObject.SetActive(true);
+            if (eatCount >= 25) owans_25.gameObject.SetActive(true);
+            if (eatCount >= 35) owans_35.gameObject.SetActive(true);
+            if (eatCount >= 46) owans_46.gameObject.SetActive(true);
+
+            //まんぷく状態
+            if (manpukuCount >= 5 && currentPlayerState != PlayerState.Manpuku)
+            {
+                currentPlayerState = PlayerState.Manpuku;
+                //音鳴らす
+                if (manpukuSoundPlay == false)
+                {
+                    sound05.PlayOneShot(sound05.clip);
+                    manpukuSoundPlay = true;
+                }
+                //Manpuku = true;
+                Debug.Log("onakaippai....");
+                MoveLock = true;
+                this.transform.position = setPos;
+
+                //3秒後リリース
+                Invoke("Release", 3.0f);
+            }
+
+            //ゲーム終了
+            if (gameManager.timer < 0.0f)
+            {
+                Debug.Log("shuryo!");
+                MoveLock = true;
+                this.transform.position = setPos;
+            }
+        }
+
+    }
+
+
+    //水を飲む
+    public void DrinkWater()
+    {
+        if (currentPlayerState != PlayerState.Drink)
+        {
+            Invoke("Release2", 0.30f);
+            currentPlayerState = PlayerState.Drink;
+
+        }
+        sound01.Play();
+        sound02.Stop();
+        Debug.Log("drink water!");
+        manpukuCount--;
+    }
+
+    //joyconMODE
+    public void JoyConPlayMode()
+    {
+        if (MoveLock == false)
+        {
+            GetComponent<joyConTest>().enabled = true;
+            GetComponent<JoyconManager>().enabled = true;
+        }
+    }
+
+    //keyMODE
+    void KeyPlayMode()
+    {
+        if (MoveLock == false)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow) && currentPlayerState == PlayerState.Center)
+            {
+                if (currentPlayerState != PlayerState.Eat_Left)
+                {
+                    currentPlayerState = PlayerState.Left;
+                }
+                this.transform.position = new Vector3(-5.0f, 0.0f, 0.0f);
+            }
+            else if (Input.GetKey(KeyCode.RightArrow) && currentPlayerState == PlayerState.Center)
+            {
+                if (currentPlayerState != PlayerState.Eat_Right)
+                {
+                    currentPlayerState = PlayerState.Right;
+                }
+                this.transform.position = new Vector3(5.3f, 0.0f, 0.0f);
+            }
+            else if ((Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
+                && currentPlayerState != PlayerState.Drink)
+            {
+                currentPlayerState = PlayerState.Center;
+                this.transform.position = setPos;
+            }
+            else if (Input.GetKeyDown(KeyCode.Space) && manpukuCount != 0 && currentPlayerState == PlayerState.Center)
+            {
+                DrinkWater();
+            }
+        }
+    }
+
+    //ロック解除
+    void Release()
+    {
+        Debug.Log("Release");
+        MoveLock = false;
+        if (currentPlayerState == PlayerState.Manpuku)
+        {
+            manpukuCount = 0;
+        }
+        currentPlayerState = PlayerState.Center;
+        sound02.Stop();
+        sound05.Stop();
+        manpukuSoundPlay = false;
+    }
+
+    void Release2()
+    {
+        currentPlayerState = PlayerState.Center;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //そばを食べたら
+        if (other.gameObject.tag == "Soba" &&
+           (currentPlayerState != PlayerState.Eat_Left || currentPlayerState != PlayerState.Eat_Right))
+        {
+            if(currentPlayerState == PlayerState.Right)
+            {
+                currentPlayerState = PlayerState.Eat_Right;
+            }
+            if (currentPlayerState == PlayerState.Left)
+            {
+                currentPlayerState = PlayerState.Eat_Left;
+            }
+
+            sound03.Play();
+            sound02.Stop();
+            sound01.Stop();
+            Destroy(other.gameObject);
+            eatCount++;
+            manpukuCount++;
+            this.transform.position = setPos;
+            osumousan.transform.localScale += new Vector3(0.009f, 0.002f, 0.0f);
+        }
+
+        //辛いそばを食べたら
+        if (other.gameObject.tag == "SpicySoba" && currentPlayerState != PlayerState.Hot)
+        {
+            currentPlayerState = PlayerState.Hot;
+            MoveLock = true;
+            this.transform.position = setPos;
+            if (manpukuCount < 4)
+            {
+                Invoke("Release", 1.3f);
+            }
+            sound02.Play();
+            Debug.Log("OMG!");
+            Destroy(other.gameObject);
+            manpukuCount++;
+        }
+    }
+
+    public static int getEatCount()
+    {
+        return eatCount;
+    }
+
+}
---
+++ Assets/Scripts_Original/joyConTest.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class joyConTest : MonoBehaviour
+{
+    private static readonly Joycon.Button[] m_buttons =
+        Enum.GetValues(typeof(Joycon.Button)) as Joycon.Button[];
+
+    private List<Joycon> m_joycons;
+    private Joycon m_joyconL;
+    private Joycon m_joyconR;
+
+    public PlayerController playerController;
+    Vector3 setPos;
+
+    //bool drinkWaterReady = false;
+
+    private void Start()
+    {
+        SetControllers();
+        setPos = this.transform.position;
+    }
+
+    private void Update()
+    {
+
+        var accelL = m_joyconL.GetAccel();
+
+        //if (drinkWaterReady == false && accelL.x < 0.0f)
+        //{
+        //    drinkWaterReady = true;
+        //}
+
+        if (playerController.MoveLock == false)
+        {
+            if (accelL.y >= 0.5)
+            {
+                if (playerController.currentPlayerState != PlayerController.PlayerState.Eat_Left)
+                {
+                    playerController.currentPlayerState = PlayerController.PlayerState.Left;
+                }
+                this.transform.position = new Vector3(-5.0f, 0.0f, 0.0f);
+            }
+            else if (accelL.y <= -0.5)
+            {
+                if (playerController.currentPlayerState != PlayerController.PlayerState.Eat_Right)
+                {
+                    playerController.currentPlayerState = PlayerController.PlayerState.Right;
+                }
+                this.transform.position = new Vector3(5.0f, 0.0f, 0.0f);
+            }
+            else if (m_joyconL.GetButtonDown(Joycon.Button.DPAD_UP) && playerController.manpukuCount != 0 && this.transform.position == setPos)
+            {
+                playerController.DrinkWater();
+                playerController.currentPlayerState = PlayerController.PlayerState.Drink;
+            }
+            else if(accelL.y > 0.5 && accelL.y > -0.5 && playerController.currentPlayerState != PlayerController.PlayerState.Drink)
+            {
+                playerController.currentPlayerState = PlayerController.PlayerState.Center;
+                this.transform.position = setPos;
+            }
+        }
+
+        if (m_joycons == null || m_joycons.Count <= 0) return;
+
+        SetControllers();
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject.tag == "SpicySoba")
+        {
+            m_joyconL.SetRumble(160, 320, 0.6f, 200);
+
+        }
+    }
+
+    private void SetControllers()
+    {
+        m_joycons = JoyconManager.Instance.j;
+        if (m_joycons == null || m_joycons.Count <= 0) return;
+        m_joyconL = m_joycons.Find(c => c.isLeft);
+        m_joyconR = m_joycons.Find(c => !c.isLeft);
+    }
+}
